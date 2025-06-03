ad16f8e70a4d917094bb8c3fa256bbb16b8d6b74
saint0x <saint.0x.saint@gmail.com>
2025-06-03 18:36:56 +0000

    nix

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "A minimal test flake";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.hello ];
+        };
+        packages.default = pkgs.hello; # A simple package to build
+      }
+    );
+} 
